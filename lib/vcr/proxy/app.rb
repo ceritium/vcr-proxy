d4ca336bf3cdbf0fac173746c4b99fc518764902
Jose Galisteo <ceritium@gmail.com>
2019-11-19 15:21:01 +0100

    headers:

--- lib/vcr/proxy/app.rb
+++ lib/vcr/proxy/app.rb
@@ -15,9 +15,8 @@ class App < Sinatra::Base
     vcr_wrapper('post') do
       uri = URI("#{VCR::Proxy.config}#{request.path}")
       uri.query = URI.encode_www_form(request.params)
-      response = Faraday.post(uri, request.body.read, 'Content-Type' => 'application/json')
-      status response.status
-      response.body
+      response = Faraday.post(uri, request.body.read, request_headers)
+      relay_response(response)
     end
   end
 
@@ -25,15 +24,29 @@ class App < Sinatra::Base
     vcr_wrapper('get') do
       uri = URI("#{VCR::Proxy.config.endpoint}#{request.path}")
       uri.query = URI.encode_www_form(request.params)
-      response = Faraday.get(uri, request.params, {'Accept' => 'application/json'})
-      status response.status
-      response.body
+      response = Faraday.get(uri, request.params, request_headers)
+      relay_response(response)
     end
+  end
+
+  def relay_response(response)
+    puts response.headers.to_h
+    status response.status
+    response.body
+  end
+
+  def request_headers
+    @request_headers ||= env.select { |k, _v| k.start_with? 'HTTP_' }
+                            .collect { |key, val| [key.sub(/^HTTP_/, ''), val] }
+                            .collect { |key, val| "#{key}: #{val}<br>" }
+                            .sort
   end
 
   def vcr_wrapper(verb)
     key = "#{request.path}/#{verb}/#{request.params.to_json}"
-    VCR.use_cassette(key, match_requests_on: VCR::Proxy.config.match_requests_on) do
+    match_requests_on = VCR::Proxy.config.match_requests_on
+
+    VCR.use_cassette(key, match_requests_on: match_requests_on) do
       yield
     end
   end
--- lib/vcr/proxy/version.rb
+++ lib/vcr/proxy/version.rb
@@ -1,5 +1,5 @@
 module VCR
   module Proxy
-    VERSION = "0.1.7"
+    VERSION = "0.1.8"
   end
 end
